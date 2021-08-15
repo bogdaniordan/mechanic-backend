b3923284dd819f8a8d0aafc2af91675859f6da45
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-08-15 18:03:47 +0300

    Added upload image functionality

--- src/main/java/com/mechanicservice/service/CustomerService.java
+++ src/main/java/com/mechanicservice/service/CustomerService.java
@@ -2,20 +2,20 @@ package com.mechanicservice.service;
 
 import com.mechanicservice.aws.BucketName;
 import com.mechanicservice.aws.FileStore;
-import com.mechanicservice.model.Car;
-import com.mechanicservice.model.CardDetails;
-import com.mechanicservice.model.User;
 import com.mechanicservice.model.Customer;
-import com.mechanicservice.repository.CarRepository;
-import com.mechanicservice.repository.CardDetailsRepository;
+import com.mechanicservice.model.User;
 import com.mechanicservice.repository.CustomerRepository;
 import com.mechanicservice.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.apache.velocity.exception.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
+
+import static org.apache.http.entity.ContentType.*;
+
 
 @Service
 @AllArgsConstructor
@@ -24,6 +24,7 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final UserRepository userRepository;
     private final FileStore fileStore;
+    private final CustomerService customerService;
 
     public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
@@ -107,5 +108,53 @@ public class CustomerService {
         String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), customerId);
         String imageURL = findById(customerId).getPicture();
         return fileStore.download(path, imageURL);
+    }
+
+    void uploadUserProfileImage(Long customerId, MultipartFile file) {
+        // 1. Check if image is not empty
+        isFileEmpty(file);
+        // 2. If file is an image
+        isImage(file);
+
+        // 3. The user exists in our database
+        Customer customer = customerService.findById(customerId);
+
+        // 4. Grab some metadata from file if any
+        Map<String, String> metadata = extractMetadata(file);
+
+        // 5. Store the image in s3 and update database (userProfileImageLink) with s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), customer.getId());
+        String filename = file.getOriginalFilename();
+
+        try {
+            fileStore.save(path, filename, Optional.of(metadata), file.getInputStream());
+            customer.setPicture(filename);
+            customerRepository.save(customer);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    private Map<String, String> extractMetadata(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        return metadata;
+    }
+
+    private void isImage(MultipartFile file) {
+        if (!Arrays.asList(
+                IMAGE_JPEG.getMimeType(),
+                IMAGE_PNG.getMimeType(),
+                IMAGE_GIF.getMimeType()).contains(file.getContentType())) {
+            throw new IllegalStateException("File must be an image [" + file.getContentType() + "]");
+        }
+    }
+
+    private void isFileEmpty(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file [ " + file.getSize() + "]");
+        }
     }
 }
